3b21317e688aca242a58ec3c5a271189905df95e
Girimun <Girimun@20.20.0.124>
2017-06-13 11:12:34 -0300

    alguns testes

--- Consultorio_Medico/src/consultorio_medico/Consultorio_Medico.java
+++ Consultorio_Medico/src/consultorio_medico/Consultorio_Medico.java
@@ -1,23 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package consultorio_medico;
 
 import visao.Login;
 
-/**
- *
- * @author Mota
- */
+
 public class Consultorio_Medico {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Classe Principal
         Login telaLogin = new Login();
         telaLogin.setVisible(true);
         
